ce4d2f406ba30888c763bfcdbd10fe2bd793cf72
Istvan Balazs Opra <opra.balazs@gmail.com>
2023-02-05 11:22:51 +0900

    Add "clap" for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // Path to the input config file.
+    #[arg(short, long)]
+    config_filepath: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    dbg!(&args);
 }
