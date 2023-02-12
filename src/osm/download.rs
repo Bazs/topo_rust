c1e4be630344a33c5a3ea719d8045d810e620338
Istvan Balazs Opra <opra.balazs@gmail.com>
2023-02-12 12:34:50 +0900

    Add Rust GHA CI

--- src/osm/download.rs
+++ src/osm/download.rs
@@ -4,15 +4,34 @@ use anyhow::anyhow;
 // use std::io::Write;
 // use uuid::Uuid;
 
-pub fn download_osm_data_by_bbox(
-    left: f64,
-    bottom: f64,
-    right: f64,
-    top: f64,
-) -> anyhow::Result<String> {
+use geohash::{encode, Coord};
+
+pub struct WgsBoundingBox {
+    pub left_lon: f64,
+    pub right_lon: f64,
+    pub bottom_lat: f64,
+    pub top_lat: f64,
+}
+
+pub fn get_filename_for_bbox(bbox: &WgsBoundingBox) -> anyhow::Result<String> {
+    const GEOHASH_LENGTH: usize = 8;
+    let top_left_coord = Coord {
+        x: bbox.left_lon,
+        y: bbox.top_lat,
+    };
+    let bottom_right_coord = Coord {
+        x: bbox.right_lon,
+        y: bbox.bottom_lat,
+    };
+    let top_left_geohash = encode(top_left_coord, GEOHASH_LENGTH)?;
+    let bottom_right_geohash = encode(bottom_right_coord, GEOHASH_LENGTH)?;
+    Ok(format!("{top_left_geohash}_{bottom_right_geohash}_osm.xml"))
+}
+
+pub fn download_osm_data_by_bbox(bbox: &WgsBoundingBox) -> anyhow::Result<String> {
     let query = format!(
         "https://overpass-api.de/api/map?bbox={},{},{},{}",
-        left, bottom, right, top
+        bbox.left_lon, bbox.bottom_lat, bbox.right_lon, bbox.top_lat
     );
     let client = reqwest::blocking::Client::builder()
         .user_agent("osm-geo-mapper")
