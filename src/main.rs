879e0f498de25d7e1f3d72a587f1b390138c870d
Istvan Balazs Opra <opra.balazs@gmail.com>
2023-02-12 12:33:20 +0900

    Add geohash-based filename creation

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use anyhow::anyhow;
 use clap::Parser;
 use yaml_rust::YamlLoader;
 
-use crate::osm::download::download_osm_data_by_bbox;
+use crate::osm::download::{download_osm_data_by_bbox, WgsBoundingBox};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -26,12 +26,12 @@ fn try_main() -> anyhow::Result<()> {
     let config = &YamlLoader::load_from_str(&config_contents)?[0];
     dbg!(&config);
     let bounding_box = &config["osm_bounding_box"];
-    let osm_data = download_osm_data_by_bbox(
-        bounding_box["left_lon"].as_f64().unwrap(),
-        bounding_box["bottom_lat"].as_f64().unwrap(),
-        bounding_box["right_lon"].as_f64().unwrap(),
-        bounding_box["top_lat"].as_f64().unwrap(),
-    )?;
+    let osm_data = download_osm_data_by_bbox(&WgsBoundingBox {
+        left_lon: bounding_box["left_lon"].as_f64().unwrap(),
+        bottom_lat: bounding_box["bottom_lat"].as_f64().unwrap(),
+        right_lon: bounding_box["right_lon"].as_f64().unwrap(),
+        top_lat: bounding_box["top_lat"].as_f64().unwrap(),
+    })?;
     dbg!(&osm_data);
     Ok(())
 }
